4b9243ec7e726a8169ca8bd52ef2f72d750a4bf1
Arthur Albuquerque <arthur_albuquerque1@yahoo.com>
2026-02-03 17:40:06 -0300

    fix: change SW strategy to Network-first to solve caching issues

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'upo-app-v15';
+const CACHE_NAME = 'upo-app-v16';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -9,10 +9,9 @@ const ASSETS_TO_CACHE = [
 
 // Install Event: Cache files
 self.addEventListener('install', (event) => {
-    self.skipWaiting(); // Force active immediately
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            console.log('[Service Worker] Caching assets');
             return cache.addAll(ASSETS_TO_CACHE);
         })
     );
@@ -25,20 +24,19 @@ self.addEventListener('activate', (event) => {
             return Promise.all(
                 keyList.map((key) => {
                     if (key !== CACHE_NAME) {
-                        console.log('[Service Worker] Removing old cache', key);
                         return caches.delete(key);
                     }
                 })
             );
-        })
+        }).then(() => self.clients.claim())
     );
 });
 
-// Fetch Event: Serve from cache or network
+// Fetch Event: Network First strategy
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
         })
     );
 });
